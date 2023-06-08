675dc253bad0a6d0d051f02c70676b97a33e6a13
18a6 <ar.brhl.49@gmail.com>
2023-06-08 21:51:11 +0100

    write output to a file

--- scraper.py
+++ scraper.py
@@ -5,7 +5,8 @@ driver = webdriver.Firefox()
 driver.get("https://en.wikipedia.org/wiki/Bitcoin")
 
 elements = driver.find_elements(By.TAG_NAME, 'h1')
-for e in elements:
-    print(e.text)
+with open("output.txt", "w") as file:
+    for e in elements:
+        file.write(elements)
 
 driver.quit()
