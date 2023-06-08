fa8e3a603bc10d7ab1cea8f032428264a6eb0232
18a6 <ar.brhl.49@gmail.com>
2023-06-08 21:55:25 +0100

    added requirements

--- scraper.py
+++ scraper.py
@@ -7,6 +7,6 @@ driver.get("https://en.wikipedia.org/wiki/Bitcoin")
 elements = driver.find_elements(By.TAG_NAME, 'h1')
 with open("output.txt", "w") as file:
     for e in elements:
-        file.write(elements)
+        file.write(e)
 
 driver.quit()
